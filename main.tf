468fdf6cca20d091e95948961906e83a98c6d1ad
Kyal Lanum <kyal.lanum@whitesourcesoftware.com>
2023-01-04 12:45:57 -0700

    initial commit

---
+++ main.tf
@@ -0,0 +1,54 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+# Private module, exists within the project
+module "db_module" {
+  source = "./db"
+  
+}
+
+# Testing Kyal's Private Registry
+module "tfc-getting-started" {
+  source = "supportmendkyal.jfrog.io/terraform__hashicorp-getting-started/tfc-getting-started/aws"
+  version = "1.0"
+}
+  
+# Public module1; public, but remote tf registry, tfaws demo modules
+module "security-group_demo_mod1" {
+  source  = "terraform-aws-modules/security-group/aws//examples/complete"
+  version = "4.15.0"
+}
+
+# Public module2; public, but remote tf registry, tfaws demo modules
+module "security-group_demo_mod2" {
+  source  = "terraform-aws-modules/security-group/aws//examples/rules-only"
+  version = "4.15.0"
+}
+
+# Baked in vpc resource, invoking vpc via module is superfluous; demonstrates how provider resources are exposed
+module "vpcDemo" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.18.1"
+  cidr = "172.31.0.0/16"
+  vpc_tags = {
+    "Name" = "VPC Demo"
+  }
+}
+
+output "PrivateIP_DB" {
+  value = module.db_module.dbprivateip
+}
+
+output "DemoMod_sgdesc" {
+  value = module.security-group_demo_mod1.security_group_description
+}
+
+
+output "DemoMod_service-one-arn" {
+  value = module.security-group_demo_mod2.service_one_security_group_arn
+}
+
+output "saymyvpc" {
+  value = module.vpcDemo.vpc_cidr_block
+}
